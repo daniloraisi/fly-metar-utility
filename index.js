d6ab4b13744906065f1f0b7cf218f14610069f38
Danilo Tenorio Raisi <danilo@raisi.tech>
2021-08-31 15:45:10 -0300

    feat: Initial Commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const url = require("url");
+const puppeteer = require("puppeteer");
+
+http
+  .createServer(async function (req, res) {
+    const queryObject = url.parse(req.url, true).query;
+
+    if (!queryObject.icao) {
+      res.end("Informe um ICAO");
+      return;
+    }
+
+    const metarUrl = `https://metar-taf.com/${queryObject.icao}`;
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "disable-setupid-sandbox"],
+    });
+    const page = await browser.newPage();
+
+    try {
+      await page.goto(metarUrl);
+    } catch (e) {
+      console.log(e);
+      res.end();
+      return;
+    }
+
+    const expression =
+      "//code[@class='bg-darkblue rounded py-2 px-3 text-white d-block']";
+    const elements = await page.$x(expression);
+
+    try {
+      await page.waitForXPath(expression, { timeout: 3000 });
+    } catch (error) {
+      console.log(error);
+      res.end();
+      return;
+    }
+
+    const metar = await page.evaluate((el) => el.innerHTML, elements[0]);
+    console.log(metar);
+
+    res.end(metar);
+  })
+  .listen(3000, "0.0.0.0");
